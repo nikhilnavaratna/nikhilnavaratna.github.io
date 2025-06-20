585a125fc73dcee3234a6ed64dba761542f94944
nikhilnavaratna <65754139+nikhilnavaratna@users.noreply.github.com>
2025-06-20 00:38:57 +0000

    Deploying to gh-pages from @ nikhilnavaratna/nikhilnavaratna.github.io@199a2c0a69da5d7a04b2df4eaf874af6ebab9e26 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -378,8 +378,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-",
-          title: '',
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
